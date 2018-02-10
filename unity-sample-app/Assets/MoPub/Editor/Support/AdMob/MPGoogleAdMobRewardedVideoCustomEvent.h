7aeb78689bd96386667617a2992ed41ec5eb6b07
Andres Ornelas <andres@twitter.com>
2018-02-09 23:48:57 -0800

    Copy over full sample app

---
+++ unity-sample-app/Assets/MoPub/Editor/Support/AdMob/MPGoogleAdMobRewardedVideoCustomEvent.h
@@ -0,0 +1,9 @@
+#import "MPRewardedVideoCustomEvent.h"
+
+/*
+ * Certified with version 7.24.1 of the Google AdMob Ads SDK.
+ */
+
+@interface MPGoogleAdMobRewardedVideoCustomEvent : MPRewardedVideoCustomEvent
+
+@end
