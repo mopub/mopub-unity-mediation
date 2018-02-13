cebd63b4fc823c24bf270a705008da469996f6d2
Andres Ornelas <andres@twitter.com>
2018-02-12 17:41:12 -0800

    WIP

--- scripts/export-mediation-unity-packages.sh
+++ scripts/export-mediation-unity-packages.sh
@@ -6,37 +6,42 @@ source "$my_dir/validate.sh"
 # Exports each of the third-party network adapters     #
 # ==================================================== #
 
-OUT_DIR="`pwd`/unity-packages"
 UNITY_BIN=/Applications/Unity/Unity.app/Contents/MacOS/Unity
-DEST_PACKAGE="$OUT_DIR/$PACKAGE_NAME.unitypackage"
 PROJECT_PATH="`pwd`/unity-sample-app"
-SUPPORT_LIBS=( "AdColony" )
-#"AdMob" "Chartboost" "Facebook" "UnityAds" "Vungle" )
+OUT_DIR="`pwd`/unity-packages"
+SUPPORT_LIBS=( "AdColony" "GoogleAdMob" "Chartboost" "Facebook" "Unity" "Vungle" )
 
 
 # Generate Unity packages for all networks except Millennial
 for SUPPORT_LIB in "${SUPPORT_LIBS[@]}"
 do
-    SUPPORT_LIB_LOWERCASE=`echo "$SUPPORT_LIB" | tr '[:upper:]' '[:lower:]'`
-    IOS_ADAPTER_FOLDER="mopub-ios-mediation/$SUPPORT_LIB"
-    ANDROID_ADAPTER_JAR="mopub-android-mediation/libs/$SUPPORT_LIB_LOWERCASE-*.jar"
-    IOS_EXPORT_FOLDER="$PROJECT_PATH/Assets/MoPub/Editor/Support/$SUPPORT_LIB"
-    ANDROID_EXPORT_JAR="$PROJECT_PATH/Assets/Plugins/Android/mopub-support/libs/mopub-$SUPPORT_LIB_LOWERCASE-custom-events.jar"
+    echo "Processing ${SUPPORT_LIB}..."
+
+    IOS_ADAPTER_FOLDER="mopub-ios-mediation/${SUPPORT_LIB}"
+    IOS_EXPORT_FOLDER="Assets/MoPub/Editor/Support/${SUPPORT_LIB}"
+    SUPPORT_LIB_LOWERCASE=`echo "${SUPPORT_LIB}" | tr '[:upper:]' '[:lower:]'`
+    ANDROID_ADAPTER_JAR="mopub-android-mediation/libs/${SUPPORT_LIB_LOWERCASE}-*.jar"
+    ANDROID_EXPORT_JAR="Assets/Plugins/Android/mopub-support/libs/${SUPPORT_LIB}/mopub-${SUPPORT_LIB_LOWERCASE}-custom-events.jar"
 
     # Delete existing adapters
+    echo "Removing existing adapters..."
     rm -r $IOS_EXPORT_FOLDER 2> /dev/null
-    rm -r $ANDROID_EXPORT_FOLDER 2> /dev/null
+    rm $ANDROID_EXPORT_JAR 2> /dev/null
 
-    # Copy over new adapters
-    cp -r "$IOS_ADAPTER_FOLDER" "$IOS_EXPORT_FOLDER"
+    # # Copy over new adapters
+    echo "Copying new adapters..."
+    cp -r "${IOS_ADAPTER_FOLDER}" "${PROJECT_PATH}/${IOS_EXPORT_FOLDER}"
     validate
-    cp $ANDROID_ADAPTER_JAR $ANDROID_EXPORT_JAR
+    cp $ANDROID_ADAPTER_JAR "${PROJECT_PATH}/${ANDROID_EXPORT_JAR}"
     validate
 
     # Generate Unity package
-    # DEST_PACKAGE="$OUT_DIR/${SUPPORT_LIB}Support.unitypackage"
-    # $UNITY_BIN -projectPath $PROJECT_PATH -quit -batchmode -logFile $EXPORT_LOG -exportPackage $IOS_EXPORT_FOLDER $ANDROID_EXPORT_FOLDER $DEST_PACKAGE
-    # echo "Exported $SUPPORT_LIB (iOS: $IOS_EXPORT_FOLDER | Android: $ANDROID_EXPORT_FOLDER) to $DEST_PACKAGE"
+    echo "Exporting Unity package..."
+    echo "IOS_EXPORT_FOLDER: ${IOS_EXPORT_FOLDER}"
+    DEST_PACKAGE="${OUT_DIR}/${SUPPORT_LIB}Support.unitypackage"
+    $UNITY_BIN -projectPath $PROJECT_PATH -quit -batchmode -logFile -exportPackage ${IOS_EXPORT_FOLDER} $ANDROID_EXPORT_JAR $DEST_PACKAGE
+    validate
+    echo "Exported ${SUPPORT_LIB} (iOS: ${IOS_EXPORT_FOLDER} | Android: ${ANDROID_EXPORT_JAR}) to ${DEST_PACKAGE}"
 done
 
 
---
+++ unity-sample-app/Assets/MoPub/Editor/Support/GoogleAdMob/README.md
@@ -0,0 +1,11 @@
+# mopub-ios-mediation-Google
+
+Network SDK link - https://developers.google.com/admob/ios/download
+Supported Ad formats - Banners, interstitials, Rewarded Video, Native
+
+Certifed date - 1/23/2018
+Certified network SDK version - 7.27.0
+Certified MoPub SDK version - 4.19.0
+Certified adapter version - 7.27.0.0
+
+Changes introduced - None in the adapters.
