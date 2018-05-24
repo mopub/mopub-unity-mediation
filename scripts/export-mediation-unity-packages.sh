d0e992a961a573029331f3739ebd86d0421aac9c
Andres Ornelas <andres@twitter.com>
2018-05-24 14:07:45 -0700

    Use SDK sample app instead (#16)
    * Delete mediation sample app
    * Update export script to use SDK's sample app
    * Update README with SDK sample app testing

--- scripts/export-mediation-unity-packages.sh
+++ scripts/export-mediation-unity-packages.sh
@@ -6,13 +6,18 @@ source "$my_dir/validate.sh"
 # Exports each of the third-party network adapters     #
 # ==================================================== #
 
+# NOTE: this script requires the mopub-unity-sdk repository to be present as a sibling of this one
+
 UNITY_BIN=/Applications/Unity/Unity.app/Contents/MacOS/Unity
-PROJECT_PATH="`pwd`/unity-sample-app"
+PROJECT_PATH="`pwd`/../mopub-unity-sdk/unity-sample-app"
 OUT_DIR="`pwd`/unity-packages"
 IOS_MEDIATION_DIR="mopub-ios-mediation"
 ANDROID_MEDIATION_DIR="mopub-android-mediation"
 SUPPORT_LIBS=( "AdColony" "AdMob" "AppLovin" "Chartboost" "FacebookAudienceNetwork" "Flurry" "IronSource" "OnebyAOL" "Tapjoy" "UnityAds" "Vungle" "IronSource")
-#For new networks, create relevant android/ios directories for your network. Use ANDROID_EXPORT_DIR, IOS_EXPORT_DIR for your reference.
+
+# Check sample app under mopub-unity-sdk exists
+ls $PROJECT_PATH 1> /dev/null
+validate "Unity SDK sample app not found; is the mopub-unity-sdk repository a sibling of this one?\nExpected it to be at: ${PROJECT_PATH}"
 
 # Update mediation submodules
 cd $IOS_MEDIATION_DIR
@@ -50,8 +55,10 @@ do
 
     # Copy over new adapters
     echo "Copying new adapters..."
+    mkdir -p "${PROJECT_PATH}/${IOS_EXPORT_DIR}"
     cp -r "${IOS_ADAPTER_DIR}" "${PROJECT_PATH}/${IOS_EXPORT_DIR}"
     validate
+    mkdir -p "${PROJECT_PATH}/${ANDROID_EXPORT_DIR}"
     cp $ANDROID_ADAPTER_JAR "${PROJECT_PATH}/${ANDROID_EXPORT_DIR}/${NETWORK_ADAPTERS_NAME_LOWERCASE}-${ANDROID_ADAPTER_VERSION}.jar"
     validate
 
@@ -65,5 +72,7 @@ do
 done
 
 git add .
-echo "DONE EXPORTING UNITY PACKAGES!"
-echo "Test adapter updates via the sample app then commit and push these changes."
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+echo -e "${GREEN}DONE EXPORTING UNITY PACKAGES!${NC}"
+echo "Test adapter updates via the updated sample app (under mopub-unity-sdk/) then commit and push these changes."
